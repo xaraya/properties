e3018320e00698ab0fd19c8c2dfec7f01563adc4
mfl <mfl@netspan.ch>
2010-05-30 12:23:02 +0000

    first push of this property

---
+++ templates/includes/checkall.js
@@ -0,0 +1,31 @@
+/**
+ * check or uncheck all checkboxes in a form
+ * Example :
+ * <a href="javascript:xar_base_checkall(true)">Check All</a>
+ * <a href="javascript:xar_base_checkall(false)">Uncheck All</a>
+ */
+function listing_checkall(value) {
+    var checkboxes = document.getElementsByTagName('input');
+    for (i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].name == 'optionscheckbox') 
+            checkboxes[i].checked = value;
+    }
+}
+
+/**
+ * get the id values of all checked checkboxes below a certain elementand stick them in the target object
+ * Example :
+ * <a href="javascript:listing_getchecked(targetId)">....</a>
+ */
+function listing_getchecked(targetId) {
+
+    var checked = "";
+    var checkboxes = document.getElementsByTagName('input');
+    for (i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].name == 'optionscheckbox') 
+            if (checkboxes[i].checked) 
+                checked += checkboxes[i].id + ",";
+    }
+    target = document.getElementById(targetId);
+    target.value = checked;
+}
