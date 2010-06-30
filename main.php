e36b192b4093d11cbec387e9e95657cc3955f615
mfl <mfl@netspan.ch>
2010-06-30 12:23:59 +0000

    First commit of this property. Embeds a CodeMirror editor into your application

---
+++ main.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * CodeMirror Editor property
+ *
+ * @package properties
+ * @copyright (C) 2010 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ *
+ */
+
+/**
+ * Handle the codemirror property
+ * Utilizes JavaScript based  Editor: CodeMirror
+ *
+ * @author M. Lutolf (mfl@netspan.ch)
+ */
+sys::import('modules.base.xarproperties.textarea');
+
+class CodeMirrorProperty extends TextAreaProperty
+{
+    public $id         = 30091;
+    public $name       = 'codemirror';
+    public $desc       = 'CodeMirror';
+    public $reqmodules = array();
+    
+    function __construct(ObjectDescriptor $descriptor)
+    {
+        parent::__construct($descriptor);
+        $this->tplmodule = 'auto';
+        $this->template =  'codemirror';
+        $this->filepath   = 'auto';
+    }
+
+    public function showInput(Array $data = array())
+    {
+        $data['basepath'] = sys::code() . 'properties/codemirror/templates/includes/codemirror/';
+        return parent::showInput($data);
+    }
+}
+
+?>
