e937f12c491060eb3bd2f2223a254c8bb8552c41
mfl <mfl@netspan.ch>
2010-11-15 18:47:21 +0000

    first commit of this dataproperty

---
+++ install.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CelkoPosition Property
+ *
+ * @package properties
+ * @subpackage celkoposition property
+ * @copyright (C) 2010 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+
+sys::import('properties.celkoposition.main');
+sys::import('modules.dynamicdata.class.properties.interfaces');
+
+class CelkoPositionPropertyInstall extends CelkoPositionProperty implements iDataPropertyInstall
+{
+
+    public function install(Array $data=array())
+    {
+        $dat_file = sys::code() . 'properties/celkoposition/data/configurations-dat.xml';
+        $data = array('file' => $dat_file);
+        try {
+        $objectid = xarMod::apiFunc('dynamicdata','util','import', $data);
+        } catch (Exception $e) {
+            //
+        }
+        return true;
+    }
+    
+}
+
+?>
