36bcd364c770c0789cdf6fd861b268994ce22454
mfl <mfl@netspan.ch>
2011-05-17 09:12:12 +0000

    update copyright msg

--- displaycaptchaimage.php
+++ displaycaptchaimage.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Captcha Property
+ *
+ * @package properties
+ * @subpackage captcha property
+ * @category Third Party Xaraya Property
+ * @version 1.0.0
+ * @copyright (C) 2011 Netspan AG
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
+
     include_once('class/securimage/securimage.php');
     $img = new securimage();
     $img->show(); // alternate use:  $img->show('/path/to/background.jpg');
