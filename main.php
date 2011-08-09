59577cb97d8e2dc1790f849792bc51c629ce5400
mfl <mfl@netspan.ch>
2011-08-09 17:29:40 +0000

    make this a standalone prop

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
- * Time Property
+ * CityLocation Property
  *
  * @package properties
- * @subpackage time property
+ * @subpackage citylocation property
  * @category Third Party Xaraya Property
  * @version 1.0.0
  * @copyright (C) 2011 Netspan AG
@@ -12,183 +12,109 @@
  */
 
 sys::import('modules.dynamicdata.class.properties.base');
-sys::import('xaraya.structures.datetime');
 
-class TimeProperty extends DataProperty
+class CityLocationProperty extends DataProperty
 {
-    public $id         = 30034;
-    public $name       = 'time';
-    public $desc       = 'Time';
+    public $id         = 30075;
+    public $name       = 'citylocation';
+    public $desc       = 'CityLocation';
     public $reqmodules = array();
 
-    public $display_time_format_type = 1;
-    public $display_time_format_predef = 0;
-    public $display_time_format_custom = 'c';
+    public $country     = "";
+    public $region      = "";
+    public $cityname    = "";
+    public $citycode    = "";
 
     function __construct(ObjectDescriptor $descriptor)
     {
         parent::__construct($descriptor);
         $this->tplmodule = 'auto';
-        $this->template =  'time';
+        $this->template =  'citylocation';
         $this->filepath   = 'auto';
-
-        // Import the predefined display formats here
-        sys::import('properties.time.data.formats');
     }
 
     public function checkInput($name = '', $value = null)
     {
-        $name = empty($name) ? 'dd_'.$this->id : $name;
-        // store the fieldname for validations who need them (e.g. file uploads)
-        $this->fieldname = $name;
-        if (!isset($value)) {
-            list($isvalid, $ampm) = $this->fetchValue($name . '_ampm');
-            list($isvalid, $hours) = $this->fetchValue($name . '_hour');
-            list($isvalid, $minutes) = $this->fetchValue($name . '_minute');
-            list($isvalid, $seconds) = $this->fetchValue($name . '_second');
+        if (!xarVarFetch('country_' . $name, 'str', $country, '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('region_' . $name, 'str', $region, '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('city_' . $name, 'str', $city, '', XARVAR_DONT_REUSE)) return;
+
+        $this->country = $country;
+        $this->region = $region;
+        $this->city = $city;
+        if ($country == 'us') {
+            $codefile = sys::code() . "properties/citylocation/data/usa." . strtolower($region) . ".php";
+        } else {
+            $codefile = sys::code() . "properties/citylocation/data/" . $country . ".php";
         }
-        if (!isset($ampm) && !isset($hours) && !isset($minutes) && !isset($seconds)) {
-            $this->objectref->missingfields[] = $this->name;
-            return null;
+        if (file_exists($codefile)) {
+            include($codefile);
+            $dropdown = DataPropertyMaster::getProperty(array('name' => 'dropdown'));
+            $dropdown->options = $city_data;
+            $dropdown->value = $this->city;
+            $cityname = $dropdown->getOption();
+        } else {
+            $cityname = '';
         }
-        if ($ampm == 'pm') $hours += 12;
-        $value = $hours*3600 + $minutes*60 + $seconds;
-        return $this->validateValue($value);
+        $this->value = serialize(array('country' => $country,'region' => $region,'city' => array('name' => $cityname, 'code' => $city)));
+        return true;
     }
 
     public function showInput(Array $data = array())
     {
-        $data['value'] = $this->getvaluearray($data);
+        if (empty($data['name'])) $data['name'] = "dd_" . $this->id;
 
-        if(!isset($data['onchange'])) $data['onchange'] = null; // let tpl decide what to do
-        $data['extraparams'] =!empty($extraparams) ? $extraparams : "";
-        return DataProperty::showInput($data);
+        if (empty($data['value'])) $data['value'] = $this->value;
+        try {
+            $info = unserialize($data['value']);
+            $this->country = $info['country'];
+            $this->region = $info['region'];
+            $this->cityname = $info['city']['name'];   
+            $this->citycode = $info['city']['code'];   
+        } catch (Exception $e) {}
+
+        if (!xarVarFetch('country_' . $data['name'], 'str', $data['country'], '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('region_' . $data['name'], 'str', $data['region'], '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('city_' . $data['name'], 'str', $data['city'], '', XARVAR_DONT_REUSE)) return;
+        if (empty($data['country'])) $data['country'] = $this->country;
+        if (empty($data['region'])) $data['region'] = $this->region;
+        if (empty($data['city'])) $data['city'] = $this->citycode;
+
+        if ($data['country'] == 'us') {
+            $codefile = sys::code() . "properties/citylocation/data/usa." . strtolower($data['region']) . ".php";
+        } else {
+            $codefile = sys::code() . "properties/citylocation/data/" . $data['country'] . ".php";
+        }
+        if (file_exists($codefile)) {
+            include($codefile);
+            $data['cityoptions'] = $city_data;
+            foreach ($data['cityoptions'] as $key => $row) {
+                $id[$key]  = $row['id'];
+                $name[$key] = $row['name'];
+            }
+            array_multisort($name, SORT_ASC, $id, SORT_ASC, $data['cityoptions']);
+        } else {
+            $data['cityoptions'] = array();
+        }
+        return parent::showInput($data);
     }
 
     public function showOutput(Array $data = array())
     {
-        if (!isset($data['value'])) {
-            $value = $this->value;
-        } else {
-            $value = $data['value'];
-        }
+        try {
+            $value = unserialize($this->value);
+            $this->country = $value['country'];
+            $this->region = $value['region'];
+            $this->cityname = $value['city']['name'];   
+            $this->citycode = $value['city']['code'];   
+        } catch (Exception $e) {}
 
-        if (empty($value)) {
-            $data['value']['date'] = "";
-        } else {
-            $date = new XarDateTime();
-            if (is_array($value)) {
-                $timestamp = mktime($value['hour'],$value['minute'],$value['second'],0,0,0);
-                $data['value']['time'] = $this->format($timestamp);
-            } else {
-                $time = $this->format($value);
-                $data['value'] = array();
-                $data['value']['time'] = $time;
-            }
-        }
+        if (empty($data['country'])) $data['country'] = $this->country;
+        if (empty($data['region'])) $data['region'] = $this->region;
 
-        return DataProperty::showOutput($data);
-    }
-
-    public function getValue()
-    {
-        return $this->format($this->value);
-    }
-    
-    function getvaluearray($data)
-    {
-        if (!isset($data['value'])) $value = $this->value;
-        else $value = $data['value'];
-
-        if (is_array($value)) return $value;
-
-        $value = $value == 0 ? time() : $value;
-        $date = new xarDateTime();
-        $date->settimestamp($value);
-        $valuearray['hour'] = $date->getHour();
-        $valuearray['minute'] = $date->getMinute();
-        $valuearray['second'] = $date->getSecond();
-
-        if (isset($data['format']) && ($data['format'] == 'ampm'))
-        {
-          if ($valuearray['hour'] > 11)
-          {
-            $valuearray['hour'] -= 12;
-            $valuearray['ampm'] = 'pm';
-          } else {
-            $valuearray['ampm'] = 'am';
-          }
-        }
-        return $valuearray;
-    }
-
-    function format($value)
-    {
-        switch($this->display_time_format_type) {
-            case 1:
-            default:
-                $value = xarLocaleGetFormattedTime('short', $value, false);
-            break;
-            case 2:
-                // If no format chosen, just return the raw value
-                if (!empty($this->display_time_format_predef)) {
-                    $formats = time_formats();
-                    $value = date($formats[$this->display_time_format_predef]['format'], $value);
-                }
-            break;
-            case 3:
-                $value = date($this->display_time_format_custom, $value);
-            break;
-        }
-        return $value;
-    }
-
-    function showHidden(Array $data = array())
-    {
-        $data['name']     = !empty($data['name']) ? $data['name'] : 'dd_'.$this->id;
-        $data['id']       = !empty($data['id'])   ? $data['id']   : 'dd_'.$this->id;
-
-        $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
-        $data['value'] = $this->getvaluearray($data);
-        if(!isset($data['module']))   $data['module']   = $this->tplmodule;
-        if(!isset($data['template'])) $data['template'] = $this->template;
-        if(!isset($data['layout']))   $data['layout']   = $this->layout;
-
-        return parent::showHidden($data);
+        if (empty($data['city'])) $data['city'] = $cityname;
+        return parent::showOutput($data);
     }
 }
-
-    function options_hours()
-    {
-        $options = array();
-        for($i=0;$i<24;$i++) {
-            $hour1 = "00".$i;
-            $hour1 = substr($hour1,strlen($hour1)-2);
-            $hour2 = "00".($i+1);
-            $hour2 = substr($hour2,strlen($hour2)-2);
-            $options[] = array('id' => $i*60, 'name' => $hour1 . ':00 - ' . $hour2 . ':00');
-        }
-        return $options;
-    }
-
-    function options_halfhours()
-    {
-        $options = array();
-        for($i=0;$i<=24;$i++) {
-            $hour1 = "00".$i;
-            $hour1 = substr($hour1,strlen($hour1)-2);
-            $options[] = array('id' => 2*$i*30, 'name' => $hour1 . ':00');
-            $options[] = array('id' => (2*$i+1)*30, 'name' => $hour1 . ':30');
-            /*
-            $hour1 = "00".$i;
-            $hour1 = substr($hour1,strlen($hour1)-2);
-            $hour2 = "00".($i+1);
-            $hour2 = substr($hour2,strlen($hour2)-2);
-            $options[] = array('id' => $i*30, 'name' => $hour1 . ':00 - ' . $hour1 . ':30');
-            $options[] = array('id' => ($i+1)*30, 'name' => $hour1 . ':30 - ' . $hour2 . ':00');
-            */
-        }
-        return $options;
-    }
 
 ?>
