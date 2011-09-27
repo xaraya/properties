2adb0622e74345c76528d0af9ab90e6a71f28f65
mfl <mfl@netspan.ch>
2011-09-27 16:24:10 +0000

    first push of this property

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * CityLocation Property
+ * ObjectItem Property
  *
  * @package properties
- * @subpackage citylocation property
+ * @subpackage objectitem property
  * @category Third Party Xaraya Property
  * @version 1.0.0
  * @copyright (C) 2011 Netspan AG
@@ -13,15 +13,15 @@
 
 sys::import('modules.dynamicdata.class.properties.base');
 
-class CityLocationProperty extends DataProperty
+class ObjectItemProperty extends DataProperty
 {
-    public $id         = 30075;
-    public $name       = 'citylocation';
-    public $desc       = 'CityLocation';
+    public $id         = 30115;
+    public $name       = 'objectitem';
+    public $desc       = 'ObjectItem';
     public $reqmodules = array();
 
-    public $country     = "";
-    public $region      = "";
+    public $object     = "";
+    public $item       = 0;
     public $cityname    = "";
     public $citycode    = "";
 
@@ -29,90 +29,72 @@ class CityLocationProperty extends DataProperty
     {
         parent::__construct($descriptor);
         $this->tplmodule = 'auto';
-        $this->template =  'citylocation';
+        $this->template =  'objectitem';
         $this->filepath   = 'auto';
     }
 
     public function checkInput($name = '', $value = null)
     {
-        if (!xarVarFetch('country_' . $name, 'str', $country, '', XARVAR_DONT_REUSE)) return;
-        if (!xarVarFetch('region_' . $name, 'str', $region, '', XARVAR_DONT_REUSE)) return;
-        if (!xarVarFetch('city_' . $name, 'str', $city, '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('object_' . $name, 'str', $object, '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('item_' . $name,   'str', $item, '', XARVAR_DONT_REUSE)) return;
 
-        $this->country = $country;
-        $this->region = $region;
-        $this->city = $city;
-        if ($country == 'us') {
-            $codefile = sys::code() . "properties/citylocation/data/usa." . strtolower($region) . ".php";
-        } else {
-            $codefile = sys::code() . "properties/citylocation/data/" . $country . ".php";
-        }
-        if (file_exists($codefile)) {
-            include($codefile);
-            $dropdown = DataPropertyMaster::getProperty(array('name' => 'dropdown'));
-            $dropdown->options = $city_data;
-            $dropdown->value = $this->city;
-            $cityname = $dropdown->getOption();
-        } else {
-            $cityname = '';
-        }
-        $this->value = serialize(array('country' => $country,'region' => $region,'city' => array('name' => $cityname, 'code' => $city)));
+        $this->object = $object;
+        $this->item = $item;
+
+        $this->value = $this->object . ":" . $this->item;
         return true;
     }
 
     public function showInput(Array $data = array())
     {
         if (empty($data['name'])) $data['name'] = "dd_" . $this->id;
+        if (!isset($data['value'])) $data['value'] = $this->value;
 
-        if (empty($data['value'])) $data['value'] = $this->value;
         try {
-            $info = unserialize($data['value']);
-            $this->country = $info['country'];
-            $this->region = $info['region'];
-            $this->cityname = $info['city']['name'];   
-            $this->citycode = $info['city']['code'];   
+            $value = explode(':',$data['value']);
+            $this->object = $value[0];
+            if (!isset($value[1])) $value[1] = 0;
+            $this->item = $value[1];
         } catch (Exception $e) {}
 
-        if (!xarVarFetch('country_' . $data['name'], 'str', $data['country'], '', XARVAR_DONT_REUSE)) return;
-        if (!xarVarFetch('region_' . $data['name'], 'str', $data['region'], '', XARVAR_DONT_REUSE)) return;
-        if (!xarVarFetch('city_' . $data['name'], 'str', $data['city'], '', XARVAR_DONT_REUSE)) return;
-        if (empty($data['country'])) $data['country'] = $this->country;
-        if (empty($data['region'])) $data['region'] = $this->region;
-        if (empty($data['city'])) $data['city'] = $this->citycode;
+        if (empty($data['object'])) $data['object'] = $this->object;
+        if (empty($data['item'])) $data['item'] = $this->item;
 
-        if ($data['country'] == 'us') {
-            $codefile = sys::code() . "properties/citylocation/data/usa." . strtolower($data['region']) . ".php";
+        if (!xarVarFetch('object_' . $data['name'], 'str', $data['object'], '', XARVAR_DONT_REUSE)) return;
+        if (!xarVarFetch('item_' . $data['name'],   'str', $data['item'], '', XARVAR_DONT_REUSE)) return;
+        if (empty($data['object'])) $data['object'] = $this->object;
+        if (empty($data['item'])) $data['item'] = $this->item;
+        
+        $object = DataObjectMaster::getObject(array('name' => $data['object']));
+        if (isset($object->properties['name'])) {
+            $data['item_display_prop'] = "name";
         } else {
-            $codefile = sys::code() . "properties/citylocation/data/" . $data['country'] . ".php";
+            $data['item_display_prop'] = "id";
         }
-        if (file_exists($codefile)) {
-            include($codefile);
-            $data['cityoptions'] = $city_data;
-            foreach ($data['cityoptions'] as $key => $row) {
-                $id[$key]  = $row['id'];
-                $name[$key] = $row['name'];
-            }
-            array_multisort($name, SORT_ASC, $id, SORT_ASC, $data['cityoptions']);
-        } else {
-            $data['cityoptions'] = array();
-        }
+
         return parent::showInput($data);
     }
 
     public function showOutput(Array $data = array())
     {
+        if (!isset($data['value'])) $data['value'] = $this->value;
         try {
-            $value = unserialize($this->value);
-            $this->country = $value['country'];
-            $this->region = $value['region'];
-            $this->cityname = $value['city']['name'];   
-            $this->citycode = $value['city']['code'];   
+            $value = explode(':',$data['value']);
+            $this->object = $value[0];
+            if (!isset($value[1])) $value[1] = 0;
+            $this->item = $value[1];
         } catch (Exception $e) {}
 
-        if (empty($data['country'])) $data['country'] = $this->country;
-        if (empty($data['region'])) $data['region'] = $this->region;
+        if (empty($data['object'])) $data['object'] = $this->object;
+        if (empty($data['item'])) $data['item'] = $this->item;
 
-        if (empty($data['city'])) $data['city'] = $cityname;
+        $object = DataObjectMaster::getObject(array('name' => $data['object']));
+        if (isset($object->properties['name'])) {
+            $data['item_display_prop'] = "name";
+        } else {
+            $data['item_display_prop'] = "id";
+        }
+
         return parent::showOutput($data);
     }
 }
