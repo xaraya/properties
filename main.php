afd15fb0ef6a5608a2689cac3ce5107245786e0c
Marc Lutolf <mfl@netspan.ch>
2015-04-04 10:13:50 +0200

    More of the same

--- main.php
+++ main.php
@@ -55,7 +55,8 @@ class CounterProperty extends TextBoxProperty
         // The variable the counter is stored in
         $store  = $this->initialization_counter_store;
         // Might have a variable or function, so evaluate it
-        $store = $$store;
+        eval('$evaled = ' . $store .';');
+        $store = isset($evaled) ? $evaled : null;
         // We add a prefix to make it more human readable
         $store  = 'counter_' . $store;
         
