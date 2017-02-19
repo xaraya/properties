fbad4bae46aaa3a22b399b2263b0fffb512a24af
Marc Lutolf <mfl@netspan.ch>
2017-02-19 16:08:04 +0100

    More

--- data/formats.php
+++ data/formats.php
@@ -14,11 +14,11 @@
 function datetime_formats()
 {
     $formats[1] = array('id' => 1, 'name' => 'Universal - short', 'format' => 'Y-m-d h:i', 'useoffset' => 0);
-    $formats[1] = array('id' => 1, 'name' => 'Universal - long', 'format' => 'Y-m-d H:i', 'useoffset' => 0);
-    $formats[2] = array('id' => 2, 'name' => 'US - short', 'format' => 'm-d-Y h:i A', 'useoffset' => 0);
-    $formats[2] = array('id' => 2, 'name' => 'US - long', 'format' => 'm-d-Y H:i A', 'useoffset' => 0);
-    $formats[3] = array('id' => 3, 'name' => 'Euro - short', 'format' => 'd.m.Y h:i', 'useoffset' => 0);
-    $formats[3] = array('id' => 3, 'name' => 'Euro - long', 'format' => 'd.m.Y H:i', 'useoffset' => 0);
+    $formats[2] = array('id' => 1, 'name' => 'Universal - long', 'format' => 'Y-m-d H:i', 'useoffset' => 0);
+    $formats[3] = array('id' => 2, 'name' => 'US - short', 'format' => 'm-d-Y h:i A', 'useoffset' => 0);
+    $formats[4] = array('id' => 2, 'name' => 'US - long', 'format' => 'm-d-Y H:i A', 'useoffset' => 0);
+    $formats[5] = array('id' => 3, 'name' => 'Euro - short', 'format' => 'd.m.Y h:i', 'useoffset' => 0);
+    $formats[6] = array('id' => 3, 'name' => 'Euro - long', 'format' => 'd.m.Y H:i', 'useoffset' => 0);
 
     return $formats;
 }
