47a8bcfc5381b4510c76ce94198a6cef37828f7d
Marc Lutolf <mfl@netspan.ch>
2017-03-19 11:38:27 +0100

    Make default display to be universal short

--- main.php
+++ main.php
@@ -21,10 +21,11 @@ class DateProperty extends DataProperty
     public $reqmodules = array();
     public $basetype   = 'date';
 
-    public $display_date_format_type = 1;
-    public $display_date_format_predef = 0;
-    public $display_date_format_custom = 'c';
-    public $initialization_encrypt     = false;
+    // The default display is short uniersal display
+    public $display_datetime_format_type   = 2;
+    public $display_datetime_format_predef = 1;
+    public $display_date_format_custom     = 'c';
+    public $initialization_encrypt         = false;
     public $display_start_year;
     public $display_end_year;
 
