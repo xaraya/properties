ad6f0e9a99041fe0e8d08d25ab51803b95eaa4ba
Marc Lutolf <mfl@netspan.ch>
2017-08-01 18:13:41 +0200

    Typo

--- main.php
+++ main.php
@@ -108,7 +108,7 @@ class NumberProperty extends FloatBoxProperty
             }
             if (xarModVars::get('dynamicdata', 'debugmode') &&
                 in_array(xarUserGetVar('id'), xarConfigVars::get(null, 'Site.User.DebugAdmins'))) {
-                echo $this->formatter->getErrorMessage();
+                if (!empty($this->formatter->getErrorCode())) echo $this->formatter->getErrorMessage();
             }
             return $value;
         } else {
@@ -134,7 +134,7 @@ class NumberProperty extends FloatBoxProperty
             }
             if (xarModVars::get('dynamicdata', 'debugmode') &&
                 in_array(xarUserGetVar('id'), xarConfigVars::get(null, 'Site.User.DebugAdmins'))) {
-                echo $this->formatter->getErrorMessage();
+                if (!empty($this->formatter->getErrorCode())) echo $this->formatter->getErrorMessage();
             }
         } else {
             if (empty($value)) {
