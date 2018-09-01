840c03d32f3b22e98b7e612a6acce141ae710275
Marc Lutolf <mfl@netspan.ch>
2018-09-01 15:08:01 +0200

    Cosmetics

--- main.php
+++ main.php
@@ -130,13 +130,13 @@ class JQAddressPickerProperty extends DataProperty
         $address = urlencode($address);
         $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=" . $address;
         try {
-           $ch = curl_init();            
-           curl_setopt($ch, CURLOPT_URL, $url);            
-           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);            
-           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);            
-           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);            
-           $response = curl_exec($ch);            
-           curl_close($ch);
+            $ch = curl_init();            
+            curl_setopt($ch, CURLOPT_URL, $url);            
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);            
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);            
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);            
+            $response = curl_exec($ch);            
+            curl_close($ch);
         } catch (Exception $e) {
             $response = xarML('Connection to Google maps failed');
             return false;
