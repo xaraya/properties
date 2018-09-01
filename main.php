380159efdddf9d3be4639df1092d908946feed4e
Mohamed Bilal <bilal.m@uplogictech.com>
2018-09-01 18:17:39 +0530

    changes for google address error

--- main.php
+++ main.php
@@ -130,7 +130,13 @@ class JQAddressPickerProperty extends DataProperty
         $address = urlencode($address);
         $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=" . $address;
         try {
-            $response = file_get_contents($url);
+           $ch = curl_init();            
+           curl_setopt($ch, CURLOPT_URL, $url);            
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);            
+           curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);            
+           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);            
+           $response = curl_exec($ch);            
+           curl_close($ch);
         } catch (Exception $e) {
             $response = xarML('Connection to Google maps failed');
             return false;
